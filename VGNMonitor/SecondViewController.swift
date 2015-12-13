eb5b23a22fe38e79b5fb194696bef5526c049e9e
twauer <torsten@torstens-mbp.fritz.box>
2015-12-13 17:13:29 +0100

    Initial Commit

---
+++ VGNMonitor/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  VGNMonitor
+//
+//  Created by Torsten Wauer on 13/12/15.
+//  Copyright © 2015 twdorado. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
